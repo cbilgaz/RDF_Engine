4a368eae437f969e504322636c5284cdbce3b9d1
ozler.hakan <ozler.hakan@gmail.com>
2014-03-20 12:55:34 +0100

    Query & SparqlRulesListener

---
+++ RDF_Engine/src/ua/dbproject/parsers/Query.java
@@ -0,0 +1,10 @@
+package ua.dbproject.parsers;
+
+/**
+ * The Class Query.
+ */
+public class Query {
+
+	/** The is distinct. */
+	boolean isDistinct = false;
+}
---
+++ RDF_Engine/src/ua/dbproject/parsers/SPARQLRulesListener.java
@@ -0,0 +1,90 @@
+package ua.dbproject.parsers;
+
+import ua.dbproject.parsers.SPARQL_Parser.DolarvarContext;
+import ua.dbproject.parsers.SPARQL_Parser.ObjectContext;
+import ua.dbproject.parsers.SPARQL_Parser.PredicateContext;
+import ua.dbproject.parsers.SPARQL_Parser.QueryContext;
+import ua.dbproject.parsers.SPARQL_Parser.QuestionvarContext;
+import ua.dbproject.parsers.SPARQL_Parser.SelectQueryContext;
+import ua.dbproject.parsers.SPARQL_Parser.SubjectContext;
+import ua.dbproject.parsers.SPARQL_Parser.TriplesBlockContext;
+import ua.dbproject.parsers.SPARQL_Parser.VariablesContext;
+
+/**
+ * @author HAKAN
+ * 
+ */
+public class SPARQLRulesListener extends SPARQL_BaseListener {
+
+	Query querySparql = new Query();
+
+	@Override
+	public void exitSubject(SubjectContext ctx) {
+		// TODO Auto-generated method stub
+		super.exitSubject(ctx);
+	}
+
+	@Override
+	public void enterQuery(QueryContext ctx) {
+		// TODO Auto-generated method stub
+		super.enterQuery(ctx);
+	}
+
+	@Override
+	public void enterVariables(VariablesContext ctx) {
+		// TODO Auto-generated method stub
+		super.enterVariables(ctx);
+	}
+
+	@Override
+	public void exitVariables(VariablesContext ctx) {
+		// TODO Auto-generated method stub
+		super.exitVariables(ctx);
+	}
+
+	@Override
+	public void exitQuery(QueryContext ctx) {
+		// TODO Auto-generated method stub
+		super.exitQuery(ctx);
+	}
+
+	@Override
+	public void exitTriplesBlock(TriplesBlockContext ctx) {
+		// TODO Auto-generated method stub
+		super.exitTriplesBlock(ctx);
+	}
+
+	@Override
+	public void exitPredicate(PredicateContext ctx) {
+		// TODO Auto-generated method stub
+		super.exitPredicate(ctx);
+	}
+
+	@Override
+	public void exitSelectQuery(SelectQueryContext ctx) {
+		if (ctx.DISTINCT() != null) {
+			querySparql.isDistinct = true;
+		} else {
+			querySparql.isDistinct = false;
+		}
+	}
+
+	@Override
+	public void exitObject(ObjectContext ctx) {
+		// TODO Auto-generated method stub
+		super.exitObject(ctx);
+	}
+
+	@Override
+	public void exitQuestionvar(QuestionvarContext ctx) {
+		// TODO Auto-generated method stub
+		super.exitQuestionvar(ctx);
+	}
+
+	@Override
+	public void exitDolarvar(DolarvarContext ctx) {
+		// TODO Auto-generated method stub
+		super.exitDolarvar(ctx);
+	}
+
+}
